505518276f8f670a9092a32a167b8a752b5c018e
Hatzi2 <jonas.lind10@gmail.com>
2024-04-12 12:56:35 +0200

    final

--- SUI miniprojekt/Assets/FinalMainScript.cs
+++ SUI miniprojekt/Assets/FinalMainScript.cs
@@ -10,6 +10,9 @@ public class FinalMainScript : MonoBehaviour
     public GameObject XROrigin;
     public GameObject child;
     public GameObject mainCamera;
+    public GameObject cylinder;
+    public Material mat;
+
     public int quadX = 0;
     public int quadY = 0;
     public int quadZ = 0;
@@ -21,7 +24,12 @@ public class FinalMainScript : MonoBehaviour
     private Vector3 moveDirection; // Declare moveDirection as a member variable
     private Vector3 planeLocation;
     private float moveSpeed = 0.1f;
+    private Vector3 initialPosition;
 
+    void start()
+    {
+        initialPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -46,16 +54,53 @@ public class FinalMainScript : MonoBehaviour
         planeLocation = moveDirection*5;
         planeLocation = new Vector3(planeLocation.x+ 0.1f, planeLocation.y+0.2f, planeLocation.z);
 
+        if (XROriginPosition.y < (-5.0f))
+        {
+            Debug.Log("Fallen");
+            if (XROriginPosition.z < 30)
+            {
+                XROrigin.transform.position = initialPosition;
+            }
+            
+            
+            if (XROriginPosition.z > 30)
+            {
+                XROrigin.transform.position = new Vector3 (0,0,30);
+            }
+            
+        }
+
+        Vector3 downDirection = -Vector3.up; // Use the negative y-axis direction for global down
+
+        RaycastHit hit;
+        float maxDistance = Mathf.Infinity; // We want to cast the ray indefinitely downward
+
+        // Cast a ray from the cameraPosition downward
+
+        if (Physics.Raycast(cameraPosition, downDirection, out hit, maxDistance))
+        {
+            // If the ray hits an object, print "Hit"
+            Debug.Log("Hit");
+
+            // Change the color of the material named "mat" to a greenish tint
+            mat.color = new Color(0.8f, 1.0f, 0.8f); // Adjust the values as needed for your greenish tint
+        }
+        else {
+            mat.color = new Color(1f, 1.0f, 1f);
+        }
+
+
 
         if (Lefthand.activeSelf && RightHand.activeSelf)
         {
             viewPlane.SetActive(true);
+            //cylinder.SetActive(true);
             time += Time.deltaTime;
             
             viewPlane.transform.localPosition = mainCameraPosition+planeLocation;
             viewPlane.transform.LookAt(mainCamera.transform.position);
-            viewPlane.transform.localScale = new Vector3(distance, distance, distance);
-
+            viewPlane.transform.localScale = new Vector3(distance, distance, 0.1f);
+            cylinder.transform.localPosition = cameraPosition;
 
 
             camera.transform.LookAt(camera.transform.position + moveDirection);
@@ -65,7 +110,7 @@ public class FinalMainScript : MonoBehaviour
             Vector3 localMoveDirection = camera.transform.InverseTransformDirection(moveDirection);
 
             // Set the local position of the camera along its local z-axis
-            camera.transform.localPosition = new Vector3(0, 0, localMoveDirection.z * distance * 20);
+            camera.transform.localPosition = new Vector3(0, 0, localMoveDirection.z * distance * 30);
 
 
 
@@ -75,6 +120,7 @@ public class FinalMainScript : MonoBehaviour
         {
             // Deactivate view plane
             viewPlane.SetActive(false);
+            cylinder.SetActive(false);
 
             // If time is greater than 0.3
             if (time > 0.3)
